1c7041bbf43c6556c51cf763c2cbbdde3119775a
Marcin Krystianc <marcin.krystianc@gmail.com>
2020-10-05 12:28:50 +0200

    Adding RestoreRunner

---
+++ RestoreRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Build.Locator;
+
+namespace RestoreRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var instance in MSBuildLocator.QueryVisualStudioInstances())
+            {
+                Console.WriteLine($"MSBuildPath:{instance.MSBuildPath}, VisualStudioRootPath:{instance.VisualStudioRootPath}");
+            }
+            MSBuildLocator.RegisterDefaults();
+            var restoreRunner = new RestoreRunner();
+            restoreRunner.RunRestore();
+        }
+    }
+}
---
+++ RestoreRunner/RestoreRunner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Execution;
+using Microsoft.Build.Framework;
+
+namespace RestoreRunner
+{
+    public class RestoreRunner
+    {
+        public void RunRestore()
+        {  
+            var logger = new BasicLogger();
+
+            try
+            {
+                string projectFileName = @"C:\gr-oss\GRLargeApp\Solution.sln"; // <--- Change here can be another
+                var projectCollection = new ProjectCollection();
+                // logger.Verbosity = LoggerVerbosity.Diagnostic;
+                var buildParameters = new BuildParameters(projectCollection)
+                {
+                    Loggers = new List<Microsoft.Build.Framework.ILogger> {logger},
+                };
+
+                var globalProperty = new Dictionary<String, String>
+                {
+                };
+
+                BuildManager.DefaultBuildManager.ResetCaches();
+
+                var buildRequest =
+                    new BuildRequestData(projectFileName, globalProperty, null, new [] {"Restore"}, null);
+                var buildResult = BuildManager.DefaultBuildManager.Build(buildParameters, buildRequest);
+                if (buildResult.OverallResult == BuildResultCode.Failure)
+                {
+                    throw new Exception("Build failed");
+                }
+            }
+            finally
+            {
+                Console.WriteLine(logger.GetLogString());
+            }
+        }
+    }
+}
